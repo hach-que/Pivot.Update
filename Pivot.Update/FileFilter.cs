86d64e3a193c99ae79c27fce05f8c35add672427
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-08-13 18:41:33 +1000

    Added .pvdeploy mechanism for selective flashing.

---
+++ Pivot.Update/FileFilter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Pivot.Update
+{
+    public class FileFilter : IEnumerable<KeyValuePair<string, string>>
+    {
+        private List<string> m_SourceFiles = new List<string>();
+        private Dictionary<string, string> m_FileMappings = new Dictionary<string, string>();
+
+        public FileFilter(IEnumerable<string> filenames)
+        {
+            foreach (string s in filenames)
+                this.m_SourceFiles.Add(s);
+        }
+
+        public void ApplyInclude(string regex)
+        {
+            Regex re = new Regex(regex);
+            foreach (string s in this.m_SourceFiles)
+                if (re.IsMatch(s))
+                    this.m_FileMappings.Add(s, s);
+        }
+
+        public void ApplyExclude(string regex)
+        {
+            Regex re = new Regex(regex);
+            List<string> toRemove = new List<string>();
+            foreach (KeyValuePair<string, string> kv in this.m_FileMappings)
+                if (re.IsMatch(kv.Value))
+                    toRemove.Add(kv.Key);
+            foreach (string s in toRemove)
+                this.m_FileMappings.Remove(s);
+        }
+        
+        public void ApplyRewrite(string find, string replace)
+        {
+            Regex re = new Regex(find);
+            Dictionary<string, string> copy = new Dictionary<string, string>(this.m_FileMappings);
+            foreach (KeyValuePair<string, string> kv in copy)
+                if (re.IsMatch(kv.Value))
+                    this.m_FileMappings[kv.Key] = re.Replace(kv.Value, replace);
+        }
+
+        #region IEnumerable<KeyValuePair<string,string>> Members
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return this.m_FileMappings.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return ((System.Collections.IEnumerable)this.m_FileMappings).GetEnumerator();
+        }
+
+        #endregion
+    }
+}
--- pvctrl/Program.cs
+++ pvctrl/Program.cs
@@ -20,132 +20,183 @@ namespace Pivot.Update.Server.Control
             switch (args[0])
             {
                 case "create":
-                    if (args.Length < 2)
                     {
-                        Console.WriteLine("must have at least 2 arguments (create <appname>).");
-                        return;
-                    }
-
-                    string appname = args[1];
-                    Cache c = new Cache(false);
-
-                    if (c.Exists("server/" + appname))
-                    {
-                        Console.WriteLine("this app is already created.");
-                        return;
-                    }
-
-                    foreach (string s in GetRecursiveFilesInCwd())
-                    {
-                        Hash hash = Hash.FromFile(Path.Combine(Environment.CurrentDirectory, s));
-                        Console.WriteLine(Hash.Empty.ToString() + " => " + hash.ToString() + " " + s);
-                        c.Set<Hash>("server/" + appname + "/hashes/" + s, hash);
-                        if (c.Exists("server/" + appname + "/store/" + s))
-                            c.Delete("server/" + appname + "/store/" + s);
-                        File.Copy(Path.Combine(Environment.CurrentDirectory, s), c.GetFilePath("server/" + appname + "/store/" + s));
-                    }
-
-                    return;
-                case "flash":
-                    if (args.Length < 2)
-                    {
-                        Console.WriteLine("must have at least 2 arguments (flash <appname>).");
-                        return;
-                    }
-
-                    appname = args[1];
-                    c = new Cache(false);
-
-                    if (!c.Exists("server/" + appname))
-                    {
-                        Console.WriteLine("use create to create this app first.");
-                        return;
-                    }
-                    
-                    // Find all files in the current working directory.
-                    diff_match_patch dmf = new diff_match_patch();
-                    IEnumerable<string> envs = GetRecursiveFilesInCwd();
-                    foreach (string s in envs)
-                    {
-                        if (c.Exists("server/" + appname + "/store/" + s))
+                        if (args.Length < 2)
                         {
-                            // File is being updated, get a hash for the current version
-                            // and for the stored version.
-                            Hash oldHash = Hash.FromFile(c.GetFilePath("server/" + appname + "/store/" + s));
-                            Hash newHash = Hash.FromFile(Path.Combine(Environment.CurrentDirectory, s));
-                            if (oldHash != newHash)
+                            Console.WriteLine("must have at least 2 arguments (create <appname>).");
+                            return;
+                        }
+
+                        string appname = args[1];
+                        Cache c = new Cache(false);
+
+                        if (c.Exists("server/" + appname))
+                        {
+                            Console.WriteLine("this app is already created.");
+                            return;
+                        }
+
+                        if (!File.Exists(".pvdeploy"))
+                        {
+                            Console.WriteLine("no .pvdeploy file found; please create one.");
+                            return;
+                        }
+
+                        FileFilter filter = FileFilterParser.Parse(".pvdeploy", GetRecursiveFilesInCwd());
+                        foreach (KeyValuePair<string, string> kv in filter)
+                        {
+                            // Key is original filename.
+                            // Value is filename to store as.
+                            Hash hash = Hash.FromFile(Path.Combine(Environment.CurrentDirectory, kv.Key));
+                            Console.WriteLine(Hash.Empty.ToString() + " => " + hash.ToString() + " " + kv.Value);
+                            c.Set<Hash>("server/" + appname + "/hashes/" + kv.Value, hash);
+                            if (c.Exists("server/" + appname + "/store/" + kv.Value))
+                                c.Delete("server/" + appname + "/store/" + kv.Value);
+                            File.Copy(Path.Combine(Environment.CurrentDirectory, kv.Key), c.GetFilePath("server/" + appname + "/store/" + kv.Value));
+                        }
+
+                        return;
+                    }
+                case "test-pvdeploy":
+                    {
+                        if (args.Length < 1)
+                        {
+                            Console.WriteLine("must have at least 1 argument (test-pvdeploy).");
+                            return;
+                        }
+
+                        if (!File.Exists(".pvdeploy"))
+                        {
+                            Console.WriteLine("no .pvdeploy file found; please create one.");
+                            return;
+                        }
+
+                        FileFilter filter = FileFilterParser.Parse(".pvdeploy", GetRecursiveFilesInCwd());
+                        foreach (KeyValuePair<string, string> kv in filter)
+                        {
+                            // Key is original filename.
+                            // Value is filename to store as.
+                            Console.WriteLine(kv.Key + " => " + kv.Value);
+                        }
+
+                        return;
+                    }
+                case "flash":
+                    {
+                        if (args.Length < 2)
+                        {
+                            Console.WriteLine("must have at least 2 arguments (flash <appname>).");
+                            return;
+                        }
+
+                        string appname = args[1];
+                        Cache c = new Cache(false);
+
+                        if (!c.Exists("server/" + appname))
+                        {
+                            Console.WriteLine("use create to create this app first.");
+                            return;
+                        }
+
+                        if (!File.Exists(".pvdeploy"))
+                        {
+                            Console.WriteLine("no .pvdeploy file found; please create one.");
+                            return;
+                        }
+
+                        // Find all files in the current working directory.
+                        diff_match_patch dmf = new diff_match_patch();
+                        FileFilter filter = FileFilterParser.Parse(".pvdeploy", GetRecursiveFilesInCwd());
+                        foreach (KeyValuePair<string, string> kv in filter)
+                        {
+                            if (c.Exists("server/" + appname + "/store/" + kv.Value))
                             {
-                                // Files are different, produce a diff.
-                                byte[] oldData = FileUtils.GetAllBytes(c.GetFilePath("server/" + appname + "/store/" + s));
-                                byte[] newData = FileUtils.GetAllBytes(Path.Combine(Environment.CurrentDirectory, s));
-                                List<Patch> patches = dmf.patch_make(Encoding.ASCII.GetString(oldData), Encoding.ASCII.GetString(newData));
+                                // File is being updated, get a hash for the current version
+                                // and for the stored version.
+                                Hash oldHash = Hash.FromFile(c.GetFilePath("server/" + appname + "/store/" + kv.Value));
+                                Hash newHash = Hash.FromFile(Path.Combine(Environment.CurrentDirectory, kv.Key));
+                                if (oldHash != newHash)
+                                {
+                                    // Files are different, produce a diff.
+                                    byte[] oldData = FileUtils.GetAllBytes(c.GetFilePath("server/" + appname + "/store/" + kv.Value));
+                                    byte[] newData = FileUtils.GetAllBytes(Path.Combine(Environment.CurrentDirectory, kv.Key));
+                                    List<Patch> patches = dmf.patch_make(Encoding.ASCII.GetString(oldData), Encoding.ASCII.GetString(newData));
+                                    string result = dmf.patch_toText(patches);
+                                    c.Set<string>("server/" + appname + "/patches/" + kv.Value + "/" + oldHash + "-" + newHash, result);
+                                    c.Set<Hash>("server/" + appname + "/hashes/" + kv.Value, newHash);
+                                    if (c.Exists("server/" + appname + "/store/" + kv.Value))
+                                        c.Delete("server/" + appname + "/store/" + kv.Value);
+                                    File.Copy(Path.Combine(Environment.CurrentDirectory, kv.Key), c.GetFilePath("server/" + appname + "/store/" + kv.Value));
+                                    if (!c.Exists("server/" + appname + "/store/" + kv.Value))
+                                        throw new InvalidOperationException("Unable to copy file to server store.");
+                                    Console.WriteLine(oldHash + " => " + newHash + " " + kv.Value);
+                                }
+                            }
+                            else
+                            {
+                                // A new file is being stored.
+                                Hash newHash = Hash.FromFile(Path.Combine(Environment.CurrentDirectory, kv.Key));
+                                byte[] newData = FileUtils.GetAllBytes(Path.Combine(Environment.CurrentDirectory, kv.Key));
+                                List<Patch> patches = dmf.patch_make("", Encoding.ASCII.GetString(newData));
                                 string result = dmf.patch_toText(patches);
-                                c.Set<string>("server/" + appname + "/patches/" + s + "/" + oldHash + "-" + newHash, result);
-                                c.Set<Hash>("server/" + appname + "/hashes/" + s, newHash);
-                                if (c.Exists("server/" + appname + "/store/" + s))
-                                    c.Delete("server/" + appname + "/store/" + s);
-                                File.Copy(Path.Combine(Environment.CurrentDirectory, s), c.GetFilePath("server/" + appname + "/store/" + s));
-                                Console.WriteLine(oldHash + " => " + newHash + " " + s);
+                                c.Set<string>("server/" + appname + "/patches/" + kv.Value + "/" + Hash.Empty + "-" + newHash, result);
+                                c.Set<Hash>("server/" + appname + "/hashes/" + kv.Value, newHash);
+                                if (c.Exists("server/" + appname + "/store/" + kv.Value))
+                                    c.Delete("server/" + appname + "/store/" + kv.Value);
+                                File.Copy(Path.Combine(Environment.CurrentDirectory, kv.Key), c.GetFilePath("server/" + appname + "/store/" + kv.Value));
+                                if (!c.Exists("server/" + appname + "/store/" + kv.Value))
+                                    throw new InvalidOperationException("Unable to copy file to server store.");
+                                Console.WriteLine(Hash.Empty + " => " + newHash + " " + kv.Value);
                             }
                         }
-                        else
+                        foreach (string s in c.ListRecursive("server/" + appname + "/store"))
                         {
-                            // A new file is being stored.
-                            Hash newHash = Hash.FromFile(Path.Combine(Environment.CurrentDirectory, s));
-                            byte[] newData = FileUtils.GetAllBytes(Path.Combine(Environment.CurrentDirectory, s));
-                            List<Patch> patches = dmf.patch_make("", Encoding.ASCII.GetString(newData));
-                            string result = dmf.patch_toText(patches);
-                            c.Set<string>("server/" + appname + "/patches/" + s + "/" + Hash.Empty + "-" + newHash, result);
-                            c.Set<Hash>("server/" + appname + "/hashes/" + s, newHash);
-                            if (c.Exists("server/" + appname + "/store/" + s))
-                                c.Delete("server/" + appname + "/store/" + s);
-                            File.Copy(Path.Combine(Environment.CurrentDirectory, s), c.GetFilePath("server/" + appname + "/store/" + s));
-                            Console.WriteLine(Hash.Empty + " => " + newHash + " " + s);
+                            if (filter.Count(v => v.Value == s) == 0)
+                            {
+                                // A file is being deleted.
+                                Hash oldHash = Hash.FromFile(c.GetFilePath("server/" + appname + "/store/" + s));
+                                byte[] oldData = FileUtils.GetAllBytes(c.GetFilePath("server/" + appname + "/store/" + s));
+                                List<Patch> patches = dmf.patch_make(Encoding.ASCII.GetString(oldData), "");
+                                string result = dmf.patch_toText(patches);
+                                c.Set<string>("server/" + appname + "/patches/" + s + "/" + oldHash.ToString() + "-" + Hash.Empty.ToString(), result);
+                                c.Set<Hash>("server/" + appname + "/hashes/" + s, Hash.Empty);
+                                if (c.Exists("server/" + appname + "/store/" + s))
+                                    c.Delete("server/" + appname + "/store/" + s);
+                                if (c.Exists("server/" + appname + "/store/" + s))
+                                    throw new InvalidOperationException("Unable to delete file from server store.");
+                                Console.WriteLine(oldHash + " => " + Hash.Empty + " " + s);
+                            }
                         }
-                    }
-                    foreach (string s in c.ListRecursive("server/" + appname + "/store"))
-                    {
-                        if (!envs.Contains(s))
-                        {
-                            // A file is being deleted.
-                            Hash oldHash = Hash.FromFile(c.GetFilePath("server/" + appname + "/store/" + s));
-                            byte[] oldData = FileUtils.GetAllBytes(c.GetFilePath("server/" + appname + "/store/" + s));
-                            List<Patch> patches = dmf.patch_make(Encoding.ASCII.GetString(oldData), "");
-                            string result = dmf.patch_toText(patches);
-                            c.Set<string>("server/" + appname + "/patches/" + s + "/" + oldHash.ToString() + "-" + Hash.Empty.ToString(), result);
-                            c.Set<Hash>("server/" + appname + "/hashes/" + s, Hash.Empty);
-                            if (c.Exists("server/" + appname + "/store/" + s))
-                                c.Delete("server/" + appname + "/store/" + s);
-                            Console.WriteLine(oldHash + " => " + Hash.Empty + " " + s);
-                        }
-                    }
 
-                    Console.WriteLine("flash complete.");
-                    return;
+                        Console.WriteLine("flash complete.");
+                        return;
+                    }
                 case "state":
-                    if (args.Length < 2)
                     {
-                        Console.WriteLine("must have at least 2 arguments (state <appname>).");
+                        if (args.Length < 2)
+                        {
+                            Console.WriteLine("must have at least 2 arguments (state <appname>).");
+                            return;
+                        }
+
+                        string appname = args[1];
+                        Cache c = new Cache(false);
+
+                        if (!c.Exists("server/" + appname))
+                        {
+                            Console.WriteLine("this app does not exist.");
+                            return;
+                        }
+
+                        foreach (string s in c.ListRecursive("server/" + appname + "/hashes"))
+                        {
+                            Console.WriteLine(c.Get<Hash>("server/" + appname + "/hashes/" + s) + " " + s);
+                        }
+
                         return;
                     }
-
-                    appname = args[1];
-                    c = new Cache(false);
-
-                    if (!c.Exists("server/" + appname))
-                    {
-                        Console.WriteLine("this app does not exist.");
-                        return;
-                    }
-
-                    foreach (string s in c.ListRecursive("server/" + appname + "/hashes"))
-                    {
-                        Console.WriteLine(c.Get<Hash>("server/" + appname + "/hashes/" + s) + " " + s);
-                    }
-
-                    return;
                 default:
-                    Console.WriteLine("invalid command (must be 'flash' or 'create').");
+                    Console.WriteLine("invalid command (must be 'flash', 'create', 'state' or 'test-pvdeploy').");
                     return;
             }
         }
@@ -158,7 +209,7 @@ namespace Pivot.Update.Server.Control
             
             foreach (DirectoryInfo di in current.GetDirectories())
                 foreach (string s in GetRecursiveFilesInCwd(path + "/" + di.Name))
-                    yield return (path.Substring(Environment.CurrentDirectory.Length).Replace("\\", "/").Trim('/') + "/" + di.Name + "/" + s).Trim('/');
+                    yield return (di.Name + "/" + s).Trim('/');
             foreach (FileInfo fi in current.GetFiles())
                 yield return fi.Name;
         }
